85d1ea2fe96746ffb8fffded7e998bc084013afd
Sebastian <sebasaraujo23@gmail.com>
2017-10-30 16:58:48 -0500

    alter sql
    alter sql

--- Aereopuerto/Aereopuerto-ejb/sql/ALTER PRODUCTO.sql
+++ Aereopuerto/Aereopuerto-ejb/sql/ALTER PRODUCTO.sql
@@ -1,1 +1,7 @@
 alter table producto add FECHA_PO date;
+
+alter table vuelo add hora_llegada_vu time without time zone;
+
+alter table vuelo add hora_salida_vu time without time zone;
+
+alter table vuelo drop hora_vu;
